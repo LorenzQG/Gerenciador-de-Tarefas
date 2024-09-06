0d35211af31fbc06f1e248d0c7f751c5e7c27b0b
Lorenzo <lorenzodequadrosgoncalves@gmail.com>
2024-09-06 13:25:40 -0300

    Add cookies to App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,17 +3,28 @@ import Login from "./pages/Login/Login";
 import Registro from "./pages/Registro/Registro";
 
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 export default function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+
+  function onLogin(token) {
+    setCookie("token", token, {
+      path: "/", 
+      maxAge: 28800,
+      sameSite: "none",
+    });
+  }
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-        </Routes>
-      </Router>
+      <CookiesProvider>
+        <Router>
+          <Routes>
+            {cookies.token ? <Route path="/" element={<Home token={cookies.token} removeCookie={removeCookie}/>} /> : <Route path="/" element={<Login onLogin={onLogin} />} />}
+            <Route path="/registro" element={<Registro />} />
+          </Routes>
+        </Router>
+      </CookiesProvider>
     </>
   );
 }
